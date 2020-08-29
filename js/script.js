744d8b55a5eae8dad22e0d68c29dfc151a666c9f
Viktoriya Druzhkova <62639183+Viktoriya-Druzhkova@users.noreply.github.com>
2020-08-29 14:47:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggle');
+function toggleMenu() {
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active')
+}
+
+menuToggle.addEventListener('click', toggleMenu);
+
+
+
